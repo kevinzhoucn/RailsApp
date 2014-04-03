dbe22c54d8b3b9fb282c982075ef8e53c924fccd
kevin.zhou <zzpf@163.com>
2014-04-03 15:16:39 +0800

    url

--- app/models/company.rb
+++ app/models/company.rb
@@ -7,7 +7,7 @@ class Company < ActiveRecord::Base
 
   has_attached_file :avatar, :styles => { :large => "600x400>", :medium => "300x300>", :thumb => "100x100>" }, 
                     :default_url => "/images/:style/missing.png", :whiny => false,
-                    :url => "/uploadfiles/:class/:attachment/:id_partition/:basename_:style.:extension",
+                    :url => ":rails_root/uploadfiles/:class/:attachment/:id_partition/:basename_:style.:extension",
                     :path => ":rails_root/public/uploadfiles/:class/:attachment/:id_partition/:basename_:style.:extension"
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
   validates_attachment_size :avatar, :less_than => 2.megabytes
