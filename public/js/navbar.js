34fdf64202b2827b45349460fb718cba907dd1ee
albert <albertli@foxmail.com>
2014-04-09 16:40:14 +0800

    changed layout of admin pages

--- public/js/navbar.js
+++ public/js/navbar.js
@@ -9,19 +9,27 @@ function setMenuBackground()
     
     if(/company/.test(url.toLowerCase()))
     {
-        $("#company").removeClass("inactive").addClass("active");
         $("#home").removeClass("active");
+        $("#company").removeClass("inactive").addClass("active");
     }else if(/products/.test(url.toLowerCase()))
     {
-        $("#products").removeClass("inactive").addClass("active");
         $("#home").removeClass("active");
+        $("#products").removeClass("inactive").addClass("active");
     }else if(/fields/.test(url.toLowerCase()))
     {
-        $("#fields").removeClass("inactive").addClass("active");
         $("#home").removeClass("active");
+        $("#fields").removeClass("inactive").addClass("active");
+    }else if(/intmarkets/.test(url.toLowerCase()))
+    {
+        $("#home").removeClass("active");
+        $("#internationalmarkets").removeClass("inactive").addClass("active");
+    }else if(/equipments/.test(url.toLowerCase()))
+    {
+        $("#home").removeClass("active");
+        $("#equipments").removeClass("inactive").addClass("active");
     }else if(/contactus/.test(url.toLowerCase()))
     {
-        $("#contactus").removeClass("inactive").addClass("active");
         $("#home").removeClass("active");
+        $("#contactus").removeClass("inactive").addClass("active");
     }
 }
