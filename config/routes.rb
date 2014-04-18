457511088a2172851ea6e37951cf53e7984bbcb3
albert <albertli@foxmail.com>
2014-04-18 11:00:57 +0800

    把管理界面改成中文了，同时调整了一下管理及面的外观

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ RailsApp::Application.routes.draw do
     get "/products" => :product, as: 'front_product'
     get "/intmarkets" => :market, as: 'front_market'
     get "/fields" => :field, as: 'front_field'
-    get "/equipments" => :equipment, as: 'front_equipment'
+    # equipments改名为applications #get "/equipments" => :equipment, as: 'front_equipment'
     get "/contactus" => :contact, as: 'front_contact'
     get "/admin" => :admin, as: 'front_admin'
     get "/product/ballvalve" => :productballvalve, as:'front_product_ballvalve'
