e349aeecd1b4f9e711b98a83bba1bf1cf39e6ef4
albert <albertli@foxmail.com>
2014-05-02 15:01:10 +0800

    20140502

--- config/routes.rb
+++ config/routes.rb
@@ -12,14 +12,9 @@ RailsApp::Application.routes.draw do
     get "/home" => :index
     get "/products" => :product, as: 'front_product'
     get "/intmarkets" => :market, as: 'front_market'
-    #fields 改成application了。 get "/fields" => :field, as: 'front_field'
     get "/equipments" => :equipment, as: 'front_equipment'
     get "/contactus" => :contact, as: 'front_contact'
     get "/admin" => :admin, as: 'front_admin'
-    get "/product/ballvalve" => :productballvalve, as:'front_product_ballvalve'
-    get "/product/gatevalve" => :productgatevalve, as:'front_product_gatevalve'
-    get "/product/globevalve" => :productglobevalve, as:'front_product_globevalve'
-    get "/product/checkvalve" => :productcheckvalve, as:'front_product_checkvalve'
     get "/application" => :application, as:'front_application'
   end
   namespace 'admin' do
---
+++ public/js/tupianlunbo2.js
@@ -0,0 +1,40 @@
+﻿$(document).ready(function(){
+ $('dt:first').addClass('active');
+ $('dd:first').css('display','block');
+ autoroll();
+ hookThumb();
+});
+var i=-1; //第i+1个tab开始
+var offset = 2500; //轮换时间
+var timer = null;
+function autoroll(){
+ n = $('dt').length-1;
+ i++;
+ if(i > n){
+ i = 0;
+ }
+ slide(i);
+    timer = window.setTimeout(autoroll, offset);
+ }
+function slide(i){
+ $('dt').eq(i).addClass('active').siblings().removeClass('active');
+ $('dd').eq(i).css('display','block').siblings('dd').css('display','none');
+ 
+ }
+function hookThumb(){    
+ $('dt').hover(
+  function () {
+    if (timer) {
+                clearTimeout(timer);
+    i = $(this).prevAll().length;
+             slide(i); 
+            }
+  },
+  function () {
+      
+            timer = window.setTimeout(autoroll, offset);  
+            this.blur();            
+            return false;
+  }
+); 
+}
